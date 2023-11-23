42f5ec8c2b30420a22a4ae26a26400e9fae8368f
Sohaib <alihaidercs17@gmail.com>
2023-11-23 19:34:50 +0500

    Express/Work

--- index.js
+++ index.js
@@ -2,7 +2,40 @@ const express= require('express');
 
 const path = require('path')
 
-const publicPath=path.join(__dirname,'public')
-console.log(publicPath)
-const app=express()
+const publicPath = path.join(__dirname, "public");
+const app = express();
+
+app.set("view engine", "ejs");
+
+// app.use(express.static(publicPath));
+
+app.get("", (req, res) => {
+  res.sendFile(`${publicPath}/index.html`);
+});
+
+app.get("/profile", (req, res) => {
+  const user = {
+    name: "Ali",
+    skills: ["React Natuve", "Flutter", "Node", "JavaScript", "Express"],
+  };
+  res.render("profile", { user });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${publicPath}/about.html`);
+});
+
+app.get("/help", (req, res) => {
+  res.sendFile(`${publicPath}/help.html`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(`${publicPath}/notFound.html`);
+});
+
+app.listen(5000);
 
